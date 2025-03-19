c5f2e15f267b5ecb2f7e9ae9b2edd004286aa9de
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-19 10:49:32 +0000

    created button components, modified description

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+  import HomeCalculatorButton from './components/HomeCalculatorButton.vue'
+</script>
+
 <template>
   <header class="h-max w-full px-2 py-4 md:py-8 lg:py-12">
     <nav class="mx-auto flex max-w-screen-2xl items-center gap-4">
@@ -20,11 +24,9 @@
       </h2>
       <div class="my-6 lg:my-8">
         <p class="text-lg leading-7 font-medium text-gray-400">
-          The Calculator App is a multi-functional web application built with Next.js to demonstrate
-          my skills in modern web development. It includes various calculators, such as basic,
-          scientific, Unit Converter, and BMI calculators, designed to cater to diverse user needs.
-          This project highlights clean design, responsive UI, and efficient client-side
-          calculations, making it a practical and polished showcase of my work.
+          The Calculator App is a multi-functional web application built with vue. It includes
+          various calculators, such as basic, scientific, Unit Converter, and BMI calculators,
+          designed to cater to diverse user needs.
         </p>
       </div>
     </section>
@@ -33,82 +35,10 @@
       <div
         class="my-4 grid w-full grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2 md:my-6 md:gap-4 lg:my-8 lg:gap-6"
       >
-        <a
-          href="/basic"
-          class="group flex w-full items-end gap-2.5 rounded-md border-2 border-gray-800 bg-gray-900 p-4 transition-all duration-300 hover:border-gray-700 hover:bg-gray-800"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
-          >
-            <path
-              d="M320-240h60v-80h80v-60h-80v-80h-60v80h-80v60h80v80Zm200-30h200v-60H520v60Zm0-100h200v-60H520v60Zm44-152 56-56 56 56 42-42-56-58 56-56-42-42-56 56-56-56-42 42 56 56-56 58 42 42Zm-314-70h200v-60H250v60Zm-50 472q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"
-            />
-          </svg>
-          <span
-            class="space-grotesk-500 text-gray-200 uppercase transition-all group-hover:text-gray-100"
-          >
-            basic
-          </span>
-        </a>
-        <a
-          href="/scientific"
-          class="group flex w-full items-end gap-2.5 rounded-md border-2 border-gray-800 bg-gray-900 p-4 transition-all duration-300 hover:border-gray-700 hover:bg-gray-800"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
-          >
-            <path
-              d="M200-120q-51 0-72.5-45.5T138-250l222-270v-240h-40q-17 0-28.5-11.5T280-800q0-17 11.5-28.5T320-840h320q17 0 28.5 11.5T680-800q0 17-11.5 28.5T640-760h-40v240l222 270q32 39 10.5 84.5T760-120H200Zm0-80h560L520-492v-268h-80v268L200-200Zm280-280Z"
-            />
-          </svg>
-          <span
-            class="space-grotesk-500 text-gray-200 uppercase transition-all group-hover:text-gray-100"
-          >
-            scientific
-          </span>
-        </a>
-        <a
-          href="/bmi"
-          class="group flex w-full items-end gap-2.5 rounded-md border-2 border-gray-800 bg-gray-900 p-4 transition-all duration-300 hover:border-gray-700 hover:bg-gray-800"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
-          >
-            <path
-              d="M432 32H80a48.05 48.05 0 00-48 48v352a48.05 48.05 0 0048 48h352a48.05 48.05 0 0048-48V80a48.05 48.05 0 00-48-48zm-16.71 165l-52.46 61.73a27.83 27.83 0 01-37.65 4.62c-13-9.29-39.27-24.89-69.18-24.89s-56.18 15.6-69.18 24.89a27.84 27.84 0 01-37.65-4.62L96.71 197a32.12 32.12 0 01.42-42c18.93-21.31 72.3-70.87 158.87-70.87S395.94 133.72 414.87 155a32.12 32.12 0 01.42 42z"
-            />
-          </svg>
-          <span
-            class="space-grotesk-500 text-gray-200 uppercase transition-all group-hover:text-gray-100"
-          >
-            BMI
-          </span>
-        </a>
-        <a
-          href="/unit-converter"
-          class="group flex w-full items-end gap-2.5 rounded-md border-2 border-gray-800 bg-gray-900 p-4 transition-all duration-300 hover:border-gray-700 hover:bg-gray-800"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
-          >
-            <path
-              d="M208-120q-37 0-62.5-25.5T120-208v-548q0-29 27-40.5t47 8.5l90 90-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 80 80q20 20 8.5 47T756-120H208Zm32-120h332L240-572v332Z"
-            />
-          </svg>
-          <span
-            class="space-grotesk-500 text-gray-200 uppercase transition-all group-hover:text-gray-100"
-          >
-            Unit Converter
-          </span>
-        </a>
+        <HomeCalculatorButton title="basic" link="/basic" />
+        <HomeCalculatorButton title="scientific" link="/scientific" />
+        <HomeCalculatorButton title="bmi" link="/bmi" />
+        <HomeCalculatorButton title="Unit Converter" link="/unit-converter" />
       </div>
     </section>
     <section class="mx-auto my-4 max-w-screen-md px-4 md:my-8 lg:my-12">
--- src/components/HomeCalculatorButton.vue
+++ src/components/HomeCalculatorButton.vue
@@ -1,1 +1,56 @@
-<template></template>
+<script setup lang="ts">
+  defineProps(['title', 'link'])
+</script>
+
+<template>
+  <a
+    v-bind:href="link"
+    class="group flex w-full items-end gap-2.5 rounded-md border-2 border-gray-800 bg-gray-900 p-4 transition-all duration-300 hover:border-gray-700 hover:bg-gray-800"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
+      v-if="title === 'basic'"
+    >
+      <path
+        d="M320-240h60v-80h80v-60h-80v-80h-60v80h-80v60h80v80Zm200-30h200v-60H520v60Zm0-100h200v-60H520v60Zm44-152 56-56 56 56 42-42-56-58 56-56-42-42-56 56-56-56-42 42 56 56-56 58 42 42Zm-314-70h200v-60H250v60Zm-50 472q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
+      v-else-if="title === 'scientific'"
+    >
+      <path
+        d="M200-120q-51 0-72.5-45.5T138-250l222-270v-240h-40q-17 0-28.5-11.5T280-800q0-17 11.5-28.5T320-840h320q17 0 28.5 11.5T680-800q0 17-11.5 28.5T640-760h-40v240l222 270q32 39 10.5 84.5T760-120H200Zm0-80h560L520-492v-268h-80v268L200-200Zm280-280Z"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
+      v-else-if="title === 'bmi'"
+    >
+      <path
+        d="M432 32H80a48.05 48.05 0 00-48 48v352a48.05 48.05 0 0048 48h352a48.05 48.05 0 0048-48V80a48.05 48.05 0 00-48-48zm-16.71 165l-52.46 61.73a27.83 27.83 0 01-37.65 4.62c-13-9.29-39.27-24.89-69.18-24.89s-56.18 15.6-69.18 24.89a27.84 27.84 0 01-37.65-4.62L96.71 197a32.12 32.12 0 01.42-42c18.93-21.31 72.3-70.87 158.87-70.87S395.94 133.72 414.87 155a32.12 32.12 0 01.42 42z"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      class="aspect-auto h-4 w-4 fill-gray-200 lg:h-6 lg:w-6"
+      v-else-if="title === 'Unit Converter'"
+    >
+      <path
+        d="M208-120q-37 0-62.5-25.5T120-208v-548q0-29 27-40.5t47 8.5l90 90-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 80 80q20 20 8.5 47T756-120H208Zm32-120h332L240-572v332Z"
+      />
+    </svg>
+    <span
+      class="space-grotesk-500 text-gray-200 uppercase transition-all group-hover:text-gray-100"
+    >
+      {{ title }}
+    </span>
+  </a>
+</template>
